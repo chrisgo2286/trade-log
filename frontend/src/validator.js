d3d7b248a6c24e83d2de73f593087875bb4d30ce
Christian Go <chrisgo2286@yahoo.com>
2022-03-24 16:00:22 -0400

    add more validations for NewTrade

--- frontend/src/validator.js
+++ frontend/src/validator.js
@@ -4,12 +4,24 @@ export function validate(fields) {
   var fieldErrors = {};
 
   if(fields.stock === '') {
-    fieldErrors.stock = 'Please enter a valid stock ticker!'
+    fieldErrors.stock = 'Please enter a valid stock ticker!';
   }
 
   if(!validator.isDecimal(fields.price) | fields.price === '') {
-    fieldErrors.price = 'Please enter a valid share price!'
-  }  
+    fieldErrors.price = 'Please enter a valid share price!';
+  }
+  
+  if(!validator.isInt(fields.shares) | fields.shares === '') {
+    fieldErrors.shares = 'Please enter a valid share number!';
+  }
+
+  if(fields.commission && !validator.isDecimal(fields.commission)) {
+    fieldErrors.commission = 'Please enter a valid commission!';
+  }
+
+  if(!validator.isDate(fields.date) | fields.date === '') {
+    fieldErrors.date = 'Please enter a valid date!';
+  }
 
   return fieldErrors;  
 }
