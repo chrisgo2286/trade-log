60b97f2bfcb7e58d643e296e0d16077bab82c016
Christian Go <chrisgo2286@yahoo.com>
2022-03-31 15:24:57 -0400

    completed filterByDate and filterByPrice for filterTrade function

--- frontend/src/miscScripts/filterTrades.js
+++ frontend/src/miscScripts/filterTrades.js
@@ -1,7 +1,36 @@
 export function filterTrades (trades, options) {
+  trades = (options.choice1 | options.choice2 | options.choice3) ? filterByStocks(trades, options): trades;
+  trades = (options.minPrice | options.maxPrice) ? filterByPrice(trades, options): trades;
+  trades = (options.startDate !== ''| options.endDate !== '') ? filterByDate(trades, options): trades;
+  return trades;
+}
+
+function filterByStocks (trades, options) {
   var stockChoices = []
   for(var i=1; i<4; i++) {
     ((('choice' + i) in options) === true) ? stockChoices.push(options['choice' + i]): stockChoices.push(''); 
   }
   return trades.filter(trade => stockChoices.includes(trade.stock));
+}
+
+function filterByPrice (trades, options) {
+  if(options.minPrice) {
+    trades = trades.filter(trade => parseInt(trade.price) >= parseInt(options.minPrice));
+  }
+  if(options.maxPrice) {
+    trades = trades.filter(trade => parseInt(trade.price) <= parseInt(options.maxPrice));
+  }
+  return trades;
+}
+
+function filterByDate (trades, options) {
+  if(options.startDate) {
+    const startDate = new Date(options.startDate);
+    trades = trades.filter(trade => startDate <= new Date(trade.date));
+  }
+  if(options.endDate) {
+    const endDate = new Date(options.endDate);
+    trades = trades.filter(trade => endDate >= new Date(trade.date));
+  }
+  return trades;
 }
